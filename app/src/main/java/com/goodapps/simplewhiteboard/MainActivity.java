a18be42a2e8d31b77dd5d552f9c949b5c8dc2b85
saumya <saumyakr181999@gmail.com>
2021-06-15 18:25:01 +0530

    added feature
    - change brush color

--- app/src/main/java/com/goodapps/simplewhiteboard/MainActivity.java
+++ app/src/main/java/com/goodapps/simplewhiteboard/MainActivity.java
@@ -27,6 +27,12 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private CanvasView canvasView;
     private AlertDialog.Builder currentAlertDialog;
+    AlertDialog currentDialog;
+    private SeekBar alphaSeekbar;
+    private SeekBar redSeekbar;
+    private SeekBar greenSeekbar;
+    private SeekBar blueSeekbar;
+    private View colorPrevView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,22 +44,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    private SeekBar.OnSeekBarChangeListener colorSeekbarChangeListener = new SeekBar.OnSeekBarChangeListener() {
 
-        if (item.getItemId() == R.id.clearid) {
-            canvasView.clear();
-        } else if (item.getItemId() == R.id.brushId) {
-            Log.d(TAG, "onOptionsItemSelected: Brush clicked");
-            showLineWidthDialog();
-        } else if (item.getItemId() == R.id.colorId) {
-            Log.d(TAG, "onOptionsItemSelected: color clicked");
-        } else if (item.getItemId() == R.id.saveId) {
-            Log.d(TAG, "onOptionsItemSelected: save Clicked");
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            colorPrevView.setBackgroundColor(Color.argb(
+                    alphaSeekbar.getProgress(), redSeekbar.getProgress(), greenSeekbar.getProgress(), blueSeekbar.getProgress()
+            ));
+
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -63,6 +74,21 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.clearid) {
+            canvasView.clear();
+        } else if (item.getItemId() == R.id.brushId) {
+            showLineWidthDialog();
+        } else if (item.getItemId() == R.id.colorId) {
+            showColorDialog();
+        } else if (item.getItemId() == R.id.saveId) {
+            Log.d(TAG, "onOptionsItemSelected: save Clicked");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     private void showLineWidthDialog() {
         currentAlertDialog = new AlertDialog.Builder(this);
@@ -81,12 +107,15 @@ public class MainActivity extends AppCompatActivity {
         bitmap.eraseColor(Color.WHITE);
         canvas.drawLine(20, 50, 350, 50, paint);
         widthImageView.setImageBitmap(bitmap);
-
+        widthSeekbar.setProgress(canvasView.getBrushWidth());
 
         buttonDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_LONG).show();
+                canvasView.setBrushWidth(widthSeekbar.getProgress());
+                Log.d(TAG, "onClick: currentDialog dismissed");
+                currentDialog.cancel();
+                currentAlertDialog = null;
             }
         });
 
@@ -114,8 +143,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         currentAlertDialog.setView(view);
-        currentAlertDialog.create();
-        currentAlertDialog.show();
+        currentDialog = currentAlertDialog.create();
+        currentDialog.setTitle("Change Brush Width");
+        currentDialog.show();
+    }
+
+    private void showColorDialog() {
+        currentAlertDialog = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.change_color_dialog, null);
+        alphaSeekbar = view.findViewById(R.id.alphaSeekbar);
+        redSeekbar = view.findViewById(R.id.redSeekbar);
+        greenSeekbar = view.findViewById(R.id.greenSeekbar);
+        blueSeekbar = view.findViewById(R.id.blueSeekbar);
+        colorPrevView = view.findViewById(R.id.colorPrevView);
+
+        alphaSeekbar.setOnSeekBarChangeListener(colorSeekbarChangeListener);
+        redSeekbar.setOnSeekBarChangeListener(colorSeekbarChangeListener);
+        greenSeekbar.setOnSeekBarChangeListener(colorSeekbarChangeListener);
+        blueSeekbar.setOnSeekBarChangeListener(colorSeekbarChangeListener);
+
+        // initial state of seek bars
+        int color = canvasView.getBrushColor();
+        alphaSeekbar.setProgress(Color.alpha(color));
+        redSeekbar.setProgress(Color.red(color));
+        greenSeekbar.setProgress(Color.green(color));
+        blueSeekbar.setProgress(Color.blue(color));
+
+        colorPrevView.setBackgroundColor(color);
+
+        Button btnDone = view.findViewById(R.id.colorChangeDialogBtnDone);
+
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                canvasView.setBrushColor(Color.argb(alphaSeekbar.getProgress(), redSeekbar.getProgress(), greenSeekbar.getProgress(), blueSeekbar.getProgress()));
+                currentDialog.dismiss();
+            }
+        });
+
+        currentAlertDialog.setView(view);
+        currentDialog = currentAlertDialog.create();
+        currentDialog.setTitle("Set Brush Color");
+        currentDialog.show();
+
+
     }
 
 }
